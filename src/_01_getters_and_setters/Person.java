0e8f343142406dfdf43e1190b8fae8096c5737a4
ilabstaff <ilabstaff@172.16.116.68>
2019-09-16 18:02:12 -0700

    Taco
    Person

---
+++ src/_01_getters_and_setters/Person.java
@@ -0,0 +1,22 @@
+package _01_getters_and_setters;
+
+public class Person {
+	private String name;
+	private String superpower;
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSuperpower() {
+		return superpower;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setSuperpower(String s) {
+		superpower = s;
+	}
+}
